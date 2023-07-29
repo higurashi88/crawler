6d1ad5206bb554fa9c779f58329fb95b7406bb12
higurashi88 <msippc1997@gmail.com>
2023-07-29 22:07:37 +0900

    fix api params

--- api/weather.py
+++ api/weather.py
@@ -4,7 +4,13 @@ BASE_URL = "https://api.open-meteo.com/v1/forecast"
 
 
 def get_weather(lat, lon):
-    url = f"{BASE_URL}?latitude={lat}&longitude={lon}&hourly=temperature_2m&timezone=Asia%2FTokyo"
-    response = requests.get(url)
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "hourly": "temperature_2m",
+        "timezone": "Asia/Tokyo",
+    }
+
+    response = requests.get(BASE_URL, params=params)
     data = response.json()
     return data
