638a9341a14ae4cbe5b098ce9056b037d725202f
higurashi88 <msippc1997@gmail.com>
2023-07-31 19:59:07 +0900

    refactor parameters

--- api/weather.py
+++ api/weather.py
@@ -3,13 +3,17 @@ import requests
 BASE_URL = "https://api.open-meteo.com/v1/forecast"
 
 
-def get_weather(lat, lon):
+def get_weather(lat, lon, timezone=None, hourly=False):
     params = {
         "latitude": lat,
         "longitude": lon,
-        "hourly": "temperature_2m",
-        "timezone": "Asia/Tokyo",
     }
+
+    if timezone:
+        params["timezone"] = timezone
+
+    if hourly:
+        params["hourly"] = "temperature_2m"
 
     response = requests.get(BASE_URL, params=params)
     data = response.json()
