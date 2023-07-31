ddcd15a02476613e47355e19d37dbf3e38f6c257
higurashi88 <msippc1997@gmail.com>
2023-07-31 20:06:14 +0900

    do job every 55min

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import sys
 from api.weather import get_weather
 from api.export_json import export_json
 import time
+import schedule
 
 
 def main():
+
     data = get_weather(35.6823, 179.6823, timezone="Asia/Tokyo", hourly=True)
     filepath = time.strftime("%Y%m%d%H%M%S") + ".json"
     export_json(data, filepath)
 
 
 if __name__ == '__main__':
-    main()
+    # 毎時55分に実行
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        schedule.every().hour.at(":55").do(main)
